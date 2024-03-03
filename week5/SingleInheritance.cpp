c66c75ef44c5de88f9541265ada6b60377605a1b
Vanitha295413 <158686467+Vanitha295413@users.noreply.github.com>
2024-03-03 17:29:26 +0530

    Create SingleInheritance.cpp

---
+++ week5/SingleInheritance.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class HumanParent
+{
+    public:
+        void displayBreed()
+        {
+            cout<<"I belong to the omnivore type.\nI belong to genus Homo.\n";
+        }
+        void eat()
+        {
+            cout<<"I eat both plants and meat.\n";
+        }
+
+};
+
+class HumanChild: public HumanParent
+{
+    public:
+        void play()
+        {
+            cout<<"I play a lot.\nIt helps me to relax my body and sharpen my brain.\n";
+        }
+        void skills()
+        {
+            cout<<"With skills I learn, I can do whatever I want legally.\n";
+        }
+};
+
+int main()
+{
+    HumanChild obj;
+    obj.displayBreed();
+    obj.eat();
+    obj.play();
+    obj.skills();
+    return 0;
+}
