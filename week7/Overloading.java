480d9e94636f693587e3f9ad1e4cde3bcb1ca30d
Vanitha295413 <158686467+Vanitha295413@users.noreply.github.com>
2024-03-16 20:54:07 +0530

    Create Overloading.java

---
+++ week7/Overloading.java
@@ -0,0 +1,23 @@
+class Carrom
+{
+    void play(String name1,String name2)
+    {
+        System.out.println("The Carroms played by 2 members");
+        System.out.println("1. "+name1+"\n"+"2. "+name2);
+    }
+    void play(String name1,String name2,String name3,String name4)
+    {
+        System.out.println("The Carroms played by 4 members");
+        System.out.println("1. "+name1+"\n"+"2. "+name2+"3. "+name3+"\n"+"4. "+name4);
+    }
+}
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Carrom obj=new Carrom();
+        obj.play("Vanitha","Renu");
+        obj.play("Vanitha","lavanya","Moni","Vyshnavi");
+    }
+}
